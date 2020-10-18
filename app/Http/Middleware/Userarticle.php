9a9a3a6d3db2d51c41dbc781c2c37724d18e6290
milad azar <miladazar1990@gmail.com>
2020-10-18 14:40:22 +0330

    Update Userarticle.php

--- app/Http/Middleware/Userarticle.php
+++ app/Http/Middleware/Userarticle.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Article;
 
 class Userarticle
 {
@@ -15,7 +16,6 @@ class Userarticle
      */
     public function handle($request, Closure $next)
     {
-
          $article = Article::find($request->id);
          if(!isset($article)){ 
             $response = [
