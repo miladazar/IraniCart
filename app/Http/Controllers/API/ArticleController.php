cc53e806593870c6f1aa9e4df8f1ea5a41b3b729
milad azar <miladazar1990@gmail.com>
2020-10-18 12:45:02 +0330

    Update ArticleController.php

--- app/Http/Controllers/API/ArticleController.php
+++ app/Http/Controllers/API/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Article;
 use Validator;
 
 class ArticleController extends Controller
